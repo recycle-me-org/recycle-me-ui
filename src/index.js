6dc36d0d968bc032f299f783a8aee78fe0f1a181
Anthony Iacono <72999840+anthony-iacono@users.noreply.github.com>
2021-12-03 12:26:09 -0800

    Set up Apollo Client

--- src/index.js
+++ src/index.js
@@ -4,6 +4,30 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  useQuery,
+  gql
+} from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: 'https://48p1r2roz4.sse.codesandbox.io',
+  cache: new InMemoryCache()
+});
+
+client.query({
+  query: gql`
+    query GetRates {
+      rates(currency: "USD") {
+        currency
+      }
+    }
+  `
+})
+  .then(result => console.log(result));
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
