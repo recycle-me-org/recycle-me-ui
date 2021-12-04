2bf516e066c73d691995ab0c1ebe1117c8f992f2
Sarah <sarahrudy@gmail.com>
2021-12-04 16:52:11 -0500

    Add NavBar bones and styling

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,23 @@
+import { Link } from 'react-router-dom'
+import './NavBar.css'
+import recycleIcon from '../../images/recycle.png'
+
+const NavBar = () => {
+  return (
+    <header className='nav-container'>
+      <div className='nav-icon-and-h1'>
+        <img src={ recycleIcon } className='recycle-icon' alt='recycle' />
+        <Link to='/'>
+          <h1 className='nav-h1'>Recycle Me</h1>
+        </Link>
+      </div>
+      <div className='nav-links'>
+        <p className='nav-search'>Search for Item</p>
+        <p className='nav-test'>Test Your Knowledge</p>
+        <p className='nav-creators'>Creators</p>
+      </div>
+    </header>
+  )
+}
+
+export default NavBar
