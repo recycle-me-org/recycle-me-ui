66c5338525e469f35c0523a43ec9092dd8be003a
Anthony Iacono <72999840+anthony-iacono@users.noreply.github.com>
2021-12-05 14:07:23 -0800

    Remove white space on right of page

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -3,9 +3,21 @@ import './SearchBar.css';
 const SearchBar = () => {
   return (
     <form className="search-bar">
-      <input type="text" aria-label="Zip Code" placeholder="ZIP Code"></input>
-      <input type="text" aria-label="Material" placeholder="Search for a material (plastic bottle, glass, etc.)"></input>
-      <button>Search</button>
+      <input 
+        className="search-bar__input search-bar__input--zip"
+        type="text"
+        aria-label="Zip Code"
+        placeholder="ZIP Code"
+      >
+      </input>
+      <input
+        className="search-bar__input search-bar__input--materials"
+        type="text"
+        aria-label="Material"
+        placeholder="Search for a material (plastic bottle, glass, etc.)"
+      >
+      </input>
+      <button className="search-bar__button">Search</button>
     </form>
   )
 };
