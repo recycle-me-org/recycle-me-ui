f109cebcc9d86b719621331243de06296a61ee31
Raquel Hill <raquelturing@gmail.com>
2021-12-05 13:43:06 -0700

    Hard-code Denver map coordinates

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -7,9 +7,9 @@ mapboxgl.accessToken =
 export default function Map() {
   const mapContainer = useRef(null);
   const map = useRef(null);
-  const [lng, setLng] = useState(-70.9);
-  const [lat, setLat] = useState(42.35);
-  const [zoom, setZoom] = useState(9);
+  const [lng, setLng] = useState(-104.99);
+  const [lat, setLat] = useState(39.73);
+  const [zoom, setZoom] = useState(11);
 
   useEffect(() => {
     if (map.current) return; // initialize map only once
