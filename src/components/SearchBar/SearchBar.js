930829e88af9639b5d00673e6bf41fce1e7cfed2
Anthony Iacono <72999840+anthony-iacono@users.noreply.github.com>
2021-12-05 13:11:26 -0800

    Add SearchBar

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -4,10 +4,10 @@ const SearchBar = () => {
     return (
         <div className="search-bar">
             <h2>Search</h2>
-            <label className="search-bar-input">
+            <label className="search-bar-input">ZIP
                 <input type="text"></input>
             </label>
-            <label className="search-bar-input">
+            <label className="search-bar-input">Keywords
                 <input type="text"></input>
             </label>
         </div>
