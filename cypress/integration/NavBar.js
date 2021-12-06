7ec958816fd54ff1ccac22f1ecfa07970ead651b
Anthony Iacono <72999840+anthony-iacono@users.noreply.github.com>
2021-12-06 15:32:40 -0800

    Add ip address to cypress test

--- cypress/integration/NavBar.js
+++ cypress/integration/NavBar.js
@@ -1,10 +1,10 @@
 /* eslint-disable no-undef */
 describe('NavBar', () => {
   it('Should display the site title', () => {
-    cy.visit('http://localhost:3000', () => {
+    cy.visit('http://192.168.0.3:3000', () => {
       get('cy-title').should('have.text', 'Recycle Me');
     })
   })
 
-  
+
 });
