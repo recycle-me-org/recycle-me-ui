13f12e4e7aa18cab78d0a9d327d236d776f5f78f
Raquel Hill <raquelturing@gmail.com>
2021-12-07 15:59:55 -0700

    write a before each before testing

--- cypress/integration/NavBar.js
+++ cypress/integration/NavBar.js
@@ -1,8 +1,17 @@
 /* eslint-disable no-undef */
-describe('NavBar', () => {
+// describe('NavBar', () => {
+//   it('Should display the site title', () => {
+//     cy.visit('https://recycle-me-org.github.io/recycle-me-ui/', () => {
+//       get('cy-title').should('have.text', 'Recycle Me');
+//     })
+//   })
+// });
+
+describe('Main Page User Flows', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3000');
+  });
   it('Should display the site title', () => {
-    cy.visit('https://recycle-me-org.github.io/recycle-me-ui/', () => {
-      get('cy-title').should('have.text', 'Recycle Me');
-    })
-  })
+    cy.get('cy-title');
+  });
 });
