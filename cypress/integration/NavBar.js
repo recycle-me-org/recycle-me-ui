7199ed14be35699684c18342a53f4a02347d774e
Sarah <sarahrudy@gmail.com>
2021-12-06 22:20:11 -0500

    Add / in local host

--- cypress/integration/NavBar.js
+++ cypress/integration/NavBar.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-undef */
 describe('NavBar', () => {
   it('Should display the site title', () => {
-    cy.visit('http://localhost:3000', () => {
+    cy.visit('http://localhost:3000/', () => {
       get('cy-title').should('have.text', 'Recycle Me');
     })
   })
