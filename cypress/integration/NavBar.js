10ab76a3374ad1dcaef839aa36589ee3d523d6f2
Anthony Iacono <72999840+anthony-iacono@users.noreply.github.com>
2021-12-06 22:16:16 -0800

    Replace localhost with deployed URL

--- cypress/integration/NavBar.js
+++ cypress/integration/NavBar.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-undef */
 describe('NavBar', () => {
   it('Should display the site title', () => {
-    cy.visit('http://192.168.0.3:3000', () => {
+    cy.visit('https://recycle-me-org.github.io/recycle-me-ui/', () => {
       get('cy-title').should('have.text', 'Recycle Me');
     })
   })
