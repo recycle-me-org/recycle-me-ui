56fff91059cd37f3a233170a1ae89a138054219e
Dallas Slieker <71724135+he-dslieker@users.noreply.github.com>
2021-12-07 14:09:10 -0700

    Can now run `npm start` then `npm run cy:run` to successfully run our cypress tests

--- cypress/integration/NavBar.js
+++ cypress/integration/NavBar.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-undef */
 describe('NavBar', () => {
   it('Should display the site title', () => {
-    cy.visit('https://recycle-me-org.github.io/recycle-me-ui/', () => {
+    cy.visit('/', () => {
       get('cy-title').should('have.text', 'Recycle Me');
     })
   })
