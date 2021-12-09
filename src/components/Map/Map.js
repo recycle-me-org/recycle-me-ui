885d5e75b419869875fd7062a12a74b49de87d2c
Raquel Hill <raquelturing@gmail.com>
2021-12-09 12:46:54 -0700

    Create marker component

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import GoogleMapReact from 'google-map-react';
 
-const AnyReactComponent = ({ text }) => <div>{text}</div>;
+const Map = ({ text }) => <div>{text}</div>;
 
 export default function SimpleMap() {
   const defaultProps = {
@@ -13,15 +13,27 @@ export default function SimpleMap() {
   };
 
   return (
-    // Important! Always set the container height explicitly
     <div style={{ height: '100vh', width: '100%' }}>
       <GoogleMapReact
         bootstrapURLKeys={{ key: 'AIzaSyAU7oHPj92fxgzUEeaoIEv4WqQYOBWxOJ8' }}
         defaultCenter={defaultProps.center}
         defaultZoom={defaultProps.zoom}
+        yesIWantToUseGoogleMapApiInternals
       >
-        <AnyReactComponent lat={59.955413} lng={30.337844} text="My Marker" />
+        <Map lat={59.955413} lng={30.337844} text="My Marker" />
+        <Marker lat={59.955413} lng={30.337844} />
       </GoogleMapReact>
     </div>
   );
 }
+
+// const Marker = () => {
+//   return (
+//     <>
+//       <div className="pin"></div>
+//       <div className="pulse"></div>
+//     </>
+//   );
+// };
+
+// export { Map, Marker };
