87c705e605fa39b2668b66b43c26c304a45f9c14
Raquel Hill <raquelturing@gmail.com>
2021-12-09 16:02:41 -0700

    Start map on a clean branch without the unused imports

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -1,53 +1,47 @@
-import React, { useRef, useEffect, useState } from 'react';
-import './Map.css';
-import mapboxgl from 'mapbox-gl';
-mapboxgl.accessToken =
-  'pk.eyJ1Ijoicm93ZW4wOCIsImEiOiJja3dzaW93MGEwc3E3Mm5xbHhwODl3ZHdjIn0.suCrTYweGk0ASHkCi4rmWg';
+import React from 'react';
+import {
+  GoogleMap,
+  LoadScript,
+  InfoWindow,
+  Marker,
+} from '@react-google-maps/api';
 
-export default function Map() {
-  const mapContainer = useRef(null);
-  const map = useRef(null);
-  const [lng, setLng] = useState(-104.99);
-  const [lat, setLat] = useState(39.73);
-  const [zoom, setZoom] = useState(11);
+const containerStyle = {
+  width: '800px',
+  height: '400px',
+};
 
-  useEffect(() => {
-    if (map.current) return; // initialize map only once
-    map.current = new mapboxgl.Map({
-      container: mapContainer.current,
-      style: 'mapbox://styles/mapbox/streets-v11',
-      center: [lng, lat],
-      zoom: zoom,
-    });
+const center = {
+  lat: 37.772,
+  lng: -122.214,
+};
 
-    // map.addControl(
-    //   new mapboxgl.GeolocateControl({
-    //   positionOptions: {
-    //   enableHighAccuracy: true
-    //   },
-    //   // When active the map will receive updates to the device's location as it changes.
-    //   trackUserLocation: true,
-    //   // Draw an arrow next to the location dot to indicate which direction the device is heading.
-    //   showUserHeading: true
-    //   })
-    //   );
-  });
+const position = {
+  lat: 37.772,
+  lng: -122.214,
+};
+const divStyle = {
+  background: `white`,
+  border: `1px solid #ccc`,
+  padding: 15,
+};
 
-  useEffect(() => {
-    if (!map.current) return; // wait for map to initialize
-    map.current.on('move', () => {
-      setLng(map.current.getCenter().lng.toFixed(4));
-      setLat(map.current.getCenter().lat.toFixed(4));
-      setZoom(map.current.getZoom().toFixed(2));
-    });
-  });
-
+const onLoad = (infoWindow) => {
+  console.log('infoWindow: ', infoWindow);
+};
+function MyComponent() {
   return (
-    <section className="map-display-container">
-      <div className="sidebar">
-        Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}
-      </div>
-      <div ref={mapContainer} className="map-container" />
-    </section>
+    <LoadScript googleMapsApiKey="AIzaSyAU7oHPj92fxgzUEeaoIEv4WqQYOBWxOJ8">
+      <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={10}>
+        <Marker position={position} />
+        <InfoWindow onLoad={onLoad} position={position}>
+          <div style={divStyle}>
+            <h1>InfoWindow</h1>
+          </div>
+        </InfoWindow>
+      </GoogleMap>
+    </LoadScript>
   );
 }
+
+export default React.memo(MyComponent);
