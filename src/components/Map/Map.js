f19697fafa24428cbd94963b29bda35ef902d113
Raquel Hill <raquelturing@gmail.com>
2021-12-09 14:25:19 -0700

    Create info window

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -1,27 +1,44 @@
 import React from 'react';
-import GoogleMapReact from 'google-map-react';
-import Marker from '../Marker/Marker';
+import { GoogleMap, LoadScript, InfoBox, Marker } from '@react-google-maps/api';
 
-const Map = ({ text }) => <div>{text}</div>;
+const containerStyle = {
+  width: '800px',
+  height: '400px',
+};
 
-export default function SimpleMap() {
-  const defaultProps = {
-    center: {
-      lat: 39.742043,
-      lng: -104.991531,
-    },
-    zoom: 11,
-  };
+const center = {
+  lat: 37.772,
+  lng: -122.214,
+};
+const position = {
+  lat: 37.772,
+  lng: -122.214,
+};
 
+const options = { closeBoxURL: '', enableEventPropagation: true };
+
+const onLoad = (infoBox) => {
+  console.log('infoBox: ', infoBox);
+};
+
+function MyComponent() {
   return (
-    <div style={{ height: '100vh', width: '100%' }}>
-      <GoogleMapReact
-        bootstrapURLKeys={{ key: 'AIzaSyAU7oHPj92fxgzUEeaoIEv4WqQYOBWxOJ8' }}
-        defaultCenter={defaultProps.center}
-        defaultZoom={defaultProps.zoom}
-      >
-        <Marker lat={39.742043} lng={-104.991531} />
-      </GoogleMapReact>
-    </div>
+    <LoadScript googleMapsApiKey="AIzaSyAU7oHPj92fxgzUEeaoIEv4WqQYOBWxOJ8">
+      <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={10}>
+        <Marker position={position} />
+        <InfoBox onLoad={onLoad} options={options} position={center}>
+          <div
+            style={{ backgroundColor: 'yellow', opacity: 0.75, padding: 12 }}
+          >
+            <div style={{ fontSize: 16, fontColor: `#08233B` }}>
+              Hello, World!
+            </div>
+          </div>
+        </InfoBox>
+        <></>
+      </GoogleMap>
+    </LoadScript>
   );
 }
+
+export default React.memo(MyComponent);
