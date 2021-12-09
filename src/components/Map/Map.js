7dd2fd34977442f18a90249c94b288f87c6048d5
Raquel Hill <raquelturing@gmail.com>
2021-12-08 20:29:28 -0700

    Implement new code in order to use markers

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -1,53 +1,50 @@
-import React, { useRef, useEffect, useState } from 'react';
-import './Map.css';
-import mapboxgl from 'mapbox-gl';
-mapboxgl.accessToken =
+import React, { PureComponent } from 'react';
+import ReactMapGL from 'react-map-gl';
+import { Container, Col, Row } from 'reactstrap';
+
+const mapStyle = {
+  width: '100%',
+  height: 600,
+};
+
+const mapboxApiKey =
   'pk.eyJ1Ijoicm93ZW4wOCIsImEiOiJja3dzaW93MGEwc3E3Mm5xbHhwODl3ZHdjIn0.suCrTYweGk0ASHkCi4rmWg';
 
-export default function Map() {
-  const mapContainer = useRef(null);
-  const map = useRef(null);
-  const [lng, setLng] = useState(-104.99);
-  const [lat, setLat] = useState(39.73);
-  const [zoom, setZoom] = useState(11);
+class MapView extends PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = {
+      viewport: {
+        latitude: 45.50884,
+        longitude: -73.58781,
+        zoom: 15,
+      },
+    };
+  }
 
-  useEffect(() => {
-    if (map.current) return; // initialize map only once
-    map.current = new mapboxgl.Map({
-      container: mapContainer.current,
-      style: 'mapbox://styles/mapbox/streets-v11',
-      center: [lng, lat],
-      zoom: zoom,
-    });
-
-    // map.addControl(
-    //   new mapboxgl.GeolocateControl({
-    //   positionOptions: {
-    //   enableHighAccuracy: true
-    //   },
-    //   // When active the map will receive updates to the device's location as it changes.
-    //   trackUserLocation: true,
-    //   // Draw an arrow next to the location dot to indicate which direction the device is heading.
-    //   showUserHeading: true
-    //   })
-    //   );
-  });
-
-  useEffect(() => {
-    if (!map.current) return; // wait for map to initialize
-    map.current.on('move', () => {
-      setLng(map.current.getCenter().lng.toFixed(4));
-      setLat(map.current.getCenter().lat.toFixed(4));
-      setZoom(map.current.getZoom().toFixed(2));
-    });
-  });
-
-  return (
-    <section className="map-display-container">
-      <div className="sidebar">
-        Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}
-      </div>
-      <div ref={mapContainer} className="map-container" />
-    </section>
-  );
+  render() {
+    const { viewport } = this.state;
+    return (
+      <Container fluid={true}>
+        <Row>
+          <Col>
+            <h2>Mapbox Tutorial</h2>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <ReactMapGL
+              mapboxApiAccessToken={mapboxApiKey}
+              mapStyle="mapbox://styles/mapbox/streets-v11"
+              {...viewport}
+              {...mapStyle}
+              onViewportChange={(viewport) => this.setState({ viewport })}
+            ></ReactMapGL>
+          </Col>
+        </Row>
+      </Container>
+    );
+  }
 }
+
+export default MapView;
