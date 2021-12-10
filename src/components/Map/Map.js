7d825cf6cbcd43b9fe381ab974142631ac7b6063
Raquel Hill <raquelturing@gmail.com>
2021-12-09 18:05:28 -0700

    Change the name of the map component to make it straightforward

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -29,14 +29,16 @@ const divStyle = {
 const onLoad = (infoWindow) => {
   console.log('infoWindow: ', infoWindow);
 };
-function MyComponent() {
+
+function RecyclingLocationMap() {
   return (
     <LoadScript googleMapsApiKey="AIzaSyAU7oHPj92fxgzUEeaoIEv4WqQYOBWxOJ8">
       <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={10}>
         <Marker position={position} />
         <InfoWindow onLoad={onLoad} position={position}>
           <div style={divStyle}>
-            <h1>InfoWindow</h1>
+            <h1>Name</h1>
+            <h2>Address</h2>
           </div>
         </InfoWindow>
       </GoogleMap>
@@ -44,4 +46,4 @@ function MyComponent() {
   );
 }
 
-export default React.memo(MyComponent);
+export default React.memo(RecyclingLocationMap);
