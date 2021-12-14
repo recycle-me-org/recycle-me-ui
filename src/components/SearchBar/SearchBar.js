eed426aab6dfde6ba78427ca16cc7aa6b9fdff8c
Anthony Iacono <72999840+anthony-iacono@users.noreply.github.com>
2021-12-13 23:44:43 -0800

    Resolve merge conflicts

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import Select from 'react-select';
 import './SearchBar.css';
 import { gql, useQuery } from '@apollo/client';
-import { placeIdQuery } from '../../queries/SearchLocations'
+import { placeIdQuery } from '../../queries/SearchLocations';
 
 const SearchBar = ({ client }) => {
   const [location, setLocation] = useState('');
@@ -12,39 +12,39 @@ const SearchBar = ({ client }) => {
   const [materialsOptions, setMaterialsOptions] = useState([]);
   const [materialId, setMaterialId] = useState('');
   const [locations, setLocations] = useState([]);
-  const [placeId, setPlaceId] = useState('')
+  const [placeId, setPlaceId] = useState('');
 
   useEffect(() => {
-    client.query({
-      query: gql`
-      query materials {
-        materials {
-          id
-          name
-          description
-          imageUrl
-        }
-      }
-      `,
-    }).then(data => {
-      setMaterials(data.data.materials)
-    })
-  })
+    client
+      .query({
+        query: gql`
+          query materials {
+            materials {
+              id
+              name
+            }
+          }
+        `,
+      })
+      .then((data) => {
+        setMaterials(data.data.materials);
+      });
+  });
 
   useEffect(() => {
-    const zipRegex = new RegExp(/^\d{5}$/)
+    const zipRegex = new RegExp(/^\d{5}$/);
     const timerId = setTimeout(() => {
       if (zipRegex.test(location) || !location.length) {
         setValidZip(true);
       } else {
         setValidZip(false);
       }
-    }, 500)
+    }, 500);
 
     return () => {
       clearTimeout(timerId);
-    }
-  }, [location])
+    };
+  }, [location]);
 
   const handleChange = (e) => {
     const input = e.target;
@@ -52,10 +52,10 @@ const SearchBar = ({ client }) => {
       setLocation(input.value);
     }
     if (input.name === 'materials') {
-      setStatus('')
+      setStatus('');
       setMaterials(input.value);
     }
-  }
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -65,77 +65,72 @@ const SearchBar = ({ client }) => {
     if (!location.length) {
       setValidZip(false);
     }
-  }
+  };
 
-useEffect(() => {
-  const materialData = client.cache.data.data
-  const keys = Object.keys(materialData) 
-  const materialsArray = keys.reduce((acc, currentKey) => {
-    materialData[currentKey].value = materialData[currentKey].name
-    materialData[currentKey].label = materialData[currentKey].name
-    acc.push(materialData[currentKey])
-    return acc
-  }, [])
-  setMaterialsOptions(materialsArray)
-}, [materials]);
+  useEffect(() => {
+    const materialData = client.cache.data.data;
+    const keys = Object.keys(materialData);
+    const materialsArray = keys.reduce((acc, currentKey) => {
+      materialData[currentKey].value = materialData[currentKey].name;
+      materialData[currentKey].label = materialData[currentKey].name;
+      acc.push(materialData[currentKey]);
+      return acc;
+    }, []);
+    setMaterialsOptions(materialsArray);
+  }, [materials]);
 
-
-const { data, loading, error } = useQuery(placeIdQuery, {
+  const { data, loading, error } = useQuery(placeIdQuery, {
     variables: {
-      materialId: materialId, 
-      location: location
+      materialId: '60',
+      location: '80031, United States',
+    },
+  });
+
+  // const getLocations = () => {
+  //   const locationData = `80031, United States`
+  //   console.log('materialId: ', materialId);
+  //   console.log('location: ', location)
+  //   client.query({
+  //     query: gql`
+  //       query searchLocations($materialId: String!, $location: String!) {
+  //         searchLocations(materialId: parseInt($materialId), location: $location) {
+  //           placeId
+  //         }
+  //       }
+  //     `,
+  //   }).then(data => {
+  //     console.log(data);
+  //     console.log(materialsOptions)
+  //     // setLocations(data.data.materials)
+  //   })
+  // }
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    if (validZip && materials) {
+      setMaterialId('60');
+      setLocation('80021');
     }
-  }
-);
-
-
-
-// const getLocations = () => {
-//   const locationData = `80031, United States`
-//   console.log('materialId: ', materialId);
-//   console.log('location: ', location)
-//   client.query({
-//     query: gql`
-//       query searchLocations($materialId: String!, $location: String!) {
-//         searchLocations(materialId: parseInt($materialId), location: $location) {
-//           placeId
-//         }
-//       }
-//     `,
-//   }).then(data => {
-//     console.log(data);
-//     console.log(materialsOptions)
-//     // setLocations(data.data.materials)
-//   })
-// }
-
-const handleClick = (e) => {
-  e.preventDefault();
-  if (validZip && materials) {
-    setMaterialId("60")
-    setLocation("80021")
-  }
-}
-console.log(data)
-console.log(loading)
-console.log(error)
+  };
+  console.log(data);
+  // console.log(loading);
+  // console.log(error);
 
   return (
-    <form className="search-bar" onSubmit={ (e) => handleSubmit(e) }>
-      <Select options={ materialsOptions } />
+    <form className="search-bar" onSubmit={(e) => handleSubmit(e)}>
+      <Select options={materialsOptions} />
       <div className="search-bar__input-container search-bar__input-container--zip">
-        <input 
+        <input
           className="search-bar__input"
           type="text"
           name="zip"
-          value={ location }
+          value={location}
           aria-label="ZIP Code"
           placeholder="ZIP Code"
-          onChange={ (e) => handleChange(e) }
-        >
-        </input>
-        { !validZip && <p className="error-message">Invalid ZIP code</p> }
-      </div> 
+          onChange={(e) => handleChange(e)}
+        ></input>
+        {!validZip && <p className="error-message">Invalid ZIP code</p>}
+      </div>
       {/* we may want to add some of this functionality to the select component later */}
       {/* <div className="search-bar__input-container search-bar__input-container--materials">
         <input
@@ -150,9 +145,11 @@ console.log(error)
         </input>
         { status === 'materials error' && <p className="error-message">Invalid materials</p> }
       </div> */}
-      <button onClick={ (e) => handleClick(e) } className="search-bar__button">Search</button>
+      <button onClick={(e) => handleClick(e)} className="search-bar__button">
+        Search
+      </button>
     </form>
-  )
+  );
 };
 
 export default SearchBar;
