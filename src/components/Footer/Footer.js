bab902ed4801d9e889820bc6894a6d0520212a99
Raquel Hill <raquelturing@gmail.com>
2021-12-14 15:30:33 -0700

    Hard code recycling tips and facts

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,14 +2,12 @@ import './Footer.css';
 
 const Footer = () => (
   <section className="footer-container">
-    <div className="footer-design">
-      <div className="footer-links">
-        <p className="footer-mobile-app">Mobile App</p>
-        <p className="footer-creators">Creators</p>
-      </div>
-      <div className="footer-copyright">
-        <p>©️ Recycle Me 2021</p>
-      </div>
+    <div className="footer-links">
+      <p className="footer-mobile-app">Mobile App</p>
+      <p className="footer-creators">Creators</p>
+    </div>
+    <div className="footer-copyright">
+      <p>©️ Recycle Me 2021</p>
     </div>
   </section>
 );
