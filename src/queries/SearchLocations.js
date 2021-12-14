618b50fccfc7aa384719cc8fe89dd54fe02e7188
Anthony Iacono <72999840+anthony-iacono@users.noreply.github.com>
2021-12-14 00:45:31 -0800

    Remove client from props; remove SearchLocaitons

--- src/queries/SearchLocations.js
+++
@@ -1,9 +0,0 @@
-import { gql } from '@apollo/client';
-
-export const placeIdQuery = gql`
-  query searchLocations($materialId: String!, $location: String!) {
-    searchLocations(materialId: $materialId, location: $location) {
-      placeId
-    }
-  }
-`;
