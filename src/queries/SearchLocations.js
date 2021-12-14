ba872ff952136ce64c5344173b1d820e824b22c3
Anthony Iacono <72999840+anthony-iacono@users.noreply.github.com>
2021-12-13 23:44:43 -0800

    Resolve merge conflicts

--- src/queries/SearchLocations.js
+++ src/queries/SearchLocations.js
@@ -3,7 +3,7 @@ import { gql } from '@apollo/client';
 export const placeIdQuery = gql`
   query searchLocations($materialId: String!, $location: String!) {
     searchLocations(materialId: $materialId, location: $location) {
-        placeId
+      placeId
     }
   }
-` 
+`;
