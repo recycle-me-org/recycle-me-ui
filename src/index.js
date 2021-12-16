bfc58aab00dc3bb55ef6533ffbf4d30450bd374e
Sarah <sarahrudy@gmail.com>
2021-12-16 00:15:35 -0500

    Remove mapbox

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from '../src/components/App/App';
-import 'mapbox-gl/dist/mapbox-gl.css';
 import './index.css';
 
 import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
