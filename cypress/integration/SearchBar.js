fffc4375c26a9478a2839d0d02aa49d47433e2b0
Sarah <sarahrudy@gmail.com>
2021-12-15 21:07:26 -0500

    Add test for user to type in zipcode

--- cypress/integration/SearchBar.js
+++ cypress/integration/SearchBar.js
@@ -1,4 +1,5 @@
 describe('NavBar', () => {
+
   beforeEach(() => {
     cy.visit('http://localhost:3000');
   })
@@ -13,6 +14,18 @@ describe('NavBar', () => {
           .find('input')
           .eq(1)
           .should('have.value', value)
+        cy.get(`${selector} .Select-control input`).first()
+          .click({ force: true }).type(text, { force: true })
+          .get('.Select-menu').contains(option)
+          .click({ force: true })
+          .select('#1 Plastic Bags').should('have.value', '#1 Plastic Bags')
       });
+    });
+
+    it('Should have an input container where user can type 5 digit zipcode', () => {
+      cy.get('.search-bar__input')
+        .should('have.value', '')
+        .type('80220')
+        .should('have.value', '80220')
     });
 })
